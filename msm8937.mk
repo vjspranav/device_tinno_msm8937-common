c2f0967fcd6ad1ad119ea021fb9b09f1a59be062
The-DarkBeast <pranay@firehound.org>
2019-11-03 15:20:36 +0100

    [DNM]msm8937-common: Stop building camera provider and HAL

--- msm8937.mk
+++ msm8937.mk
@@ -125,10 +125,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    #android.hardware.camera.provider@2.4-impl \
+    #android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
-    camera.msm8937 \
+    #camera.msm8937 \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
